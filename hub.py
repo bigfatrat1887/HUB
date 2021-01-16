0edababa09c83635ce4a29b840ddf00015048a98
bigfatrat1887 <63283572+bigfatrat1887@users.noreply.github.com>
2021-01-16 21:10:11 +0300

    removed searching in non-existing directory

--- hub.py
+++ hub.py
@@ -4,12 +4,12 @@ import os
 import sys
 import subprocess
 from PyQt5 import uic, QtGui
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize
 # INITIATING RICKROLL
 pygame.mixer.init()
-pygame.mixer.music.load("/MAIN_HUB/HUB_data/definatelynotarickroll.mp3")
+pygame.mixer.music.load("/HUB_data/definatelynotarickroll.mp3")
 pygame.mixer.music.set_volume(0.15)
 # gain the starting directory
 start_dir = os.getcwd()
@@ -20,23 +20,23 @@ class HUB(QWidget):
         super().__init__()
         self.count = 0
         # load UI and audio
-        uic.loadUi('D:/MAIN_HUB/HUB_data/Ui/hub.ui', self)
+        uic.loadUi('/HUB_data/Ui/hub.ui', self)
 
         # entitle the window
         self.setWindowTitle('OLD GAMES HUB')
         # create icons for buttons
-        self.setWindowIcon(QtGui.QIcon('/MAIN_HUB/HUB_data/Img/hubico.png'))
-        self.TTR.setIcon(QIcon('/MAIN_HUB/HUB_data/Img/TETRIS.png'))
+        self.setWindowIcon(QtGui.QIcon('/HUB_data/Img/hubico.png'))
+        self.TTR.setIcon(QIcon('/HUB_data/Img/TETRIS.png'))
         self.TTR.setIconSize(QSize(90, 90))
-        self.SNK.setIcon(QIcon('/MAIN_HUB/HUB_data/Img/SNAKE.png'))
+        self.SNK.setIcon(QIcon('/HUB_data/Img/SNAKE.png'))
         self.SNK.setIconSize(QSize(90, 90))
-        self.TTT.setIcon(QIcon('/MAIN_HUB/HUB_data/Img/TICTACTOE.png'))
+        self.TTT.setIcon(QIcon('/HUB_data/Img/TICTACTOE.png'))
         self.TTT.setIconSize(QSize(80, 80))
-        self.SBF.setIcon(QIcon('/MAIN_HUB/HUB_data/Img/GAMEPAD.png'))
+        self.SBF.setIcon(QIcon('/HUB_data/Img/GAMEPAD.png'))
         self.SBF.setIconSize(QSize(200, 200))
-        self.TIR.setIcon(QIcon('/MAIN_HUB/HUB_data/Img/THREE.png'))
+        self.TIR.setIcon(QIcon('/HUB_data/Img/THREE.png'))
         self.TIR.setIconSize(QSize(90, 90))
-        self.REV.setIcon(QIcon('/MAIN_HUB/HUB_data/Img/REVERSI.png'))
+        self.REV.setIcon(QIcon('/HUB_data/Img/REVERSI.png'))
         self.REV.setIconSize(QSize(70, 70))
         # make rickroll button
         self.SBF.clicked.connect(self.rolled)
